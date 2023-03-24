498b737504087bbe9ba7a4044db62c6b334902a4
GoodPants <120332064+GoodPants@users.noreply.github.com>
2023-03-24 18:09:30 -0400

    Remove readme content from top of file

--- scan-config-generator.py
+++ scan-config-generator.py
@@ -1,29 +1,3 @@
-"""
-This project is an attempt to create a tool which can generate configuration files for various network scanners and optionally allow a user to use the scans defined in the files to update a local inventory database. On first run it will identify what network interfaces on the host have IP addresses which are in private reserved IP ranges, then write the subnets for those IPs to a .json config file. Once this list of subnets is created, it uses entries in this file to build scanner configurations.
-
-Ultimately, the objective is to be able to plop a Raspberry pi or something similar on a network and be able to do basic network asset discovery, inventory management, and vulnerability tracking from a single (simple) tool on that machine.
-
-When it's finished, it should be able to do the following:
-
-    ALWAYS: 
-    1. Create a list of private subnets the host currently has an IP address on.
-    2. Create the masscan configuration file for the first scan to identify active hosts on the subnets.
-    3. Prompt user to execute the scan configured in step 2. If they confirm, then from scan output, create two configuration files:
-        3.a. A masscan config file to scan the full list of hosts identified in step 2 for commonly attacked port numbers.
-        3.b. An nmap config file to scan and fingerprint hosts identified in step 2.
-
-    OPTIONAL:
-    1. Run the scans defined in the config files from step 3 under ALWAYS
-        1.a. Masscan scan to identify hosts and ports, then store the findings in the inventory database.
-        1.b. Nmap to attempt to fingerprint specific services and software versions, then store the findings in the inventory database.
-    2. Generate a report from the inventory database
-    3. Check NIST NVD for known vulnerabilities in the service versions identified in the database and store records of CVE numbers and URLs in the database.
-
-Feature wishlist:
-* Use pycve to compare identified service versions with public CVEs in the NVD
-* Build scanner configuration files based on the existing entries in the local database
-"""
-
 # TODO: Write function to check dependencies (nmap, masscan, and psutil)
 # TODO: Write function to use masscan to create initial inventory and populate database with it.
 # TODO: Unit tests for everything
